853de057883c9c682c41292b920d8d9059c9126b
Alex Mair <alexanderjmair@gmail.com>
2024-06-26 10:45:19 +0100

    Part 2

---
+++ JavaScript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,28 @@
+"use strict"; //Makes it easier for developers to see bugs as we write and prevents some actions.
+
+/* let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("I can drive!");
+
+phrases like private and interface are reserved and thus can't be used in strict mode*/
+
+/* function logger(log) {
+  console.log(log);
+}
+
+logger("Hello");
+logger("Woah a function!");
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+ */
